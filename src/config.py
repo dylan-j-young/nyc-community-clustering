473c1314d2fe5ab0ae302c73d529feaeeba5f6ce
Dylan Young <dylan.young@colorado.edu>
2025-09-08 18:18:23 -0500

    Added shapefiles and CRSs to config.py

--- src/config.py
+++ src/config.py
@@ -8,24 +8,13 @@ import os
 # Pandas settings
 pd.set_option("display.max_columns", None)
 
-####
-# DATA PATHS
-####
-
-# Data directories
-RAW_DATA_DIR = "data/raw"
-INTERIM_DATA_DIR = "data/interim"
-PROCESSED_DATA_DIR = "data/processed"
-
-# Shapefiles
-CTRACT_SHAPEFILE = os.path.join(RAW_DATA_DIR, "shapefiles", "census-tracts", "tl_2024_36_tract.shp")
-
-# Tables
-P5_TABLE = os.path.join(RAW_DATA_DIR, "P5_race-hispanic", "DECENNIALDHC2020.P5-Data.csv")
-
 #### 
 # GEOGRAPHY
 ####
+
+# Coordinate system codes
+WEB_MERCATOR_EPSG = 3857 # Best for display
+WGS84_EPSG = 4326 # Default CRS for interoperability
 
 # FIPS county codes
 FIPS_DICT = {
@@ -35,3 +24,27 @@ FIPS_DICT = {
     "36081": "Queens",
     "36085": "Staten Island"
 }
+
+####
+# DATA PATHS
+####
+
+# Data directories
+RAW_DATA_DIR = "data/raw"
+INTERIM_DATA_DIR = "data/interim"
+PROCESSED_DATA_DIR = "data/processed"
+
+# Shapefiles: NYC census tracts (land and water)
+TRACT_SHAPEFILE = os.path.join(RAW_DATA_DIR, "shapefiles", "tl_2024_36_tract", "tl_2024_36_tract.shp")
+
+# Shapefiles: Water area polygons for each county/borough
+AREAWATER_SHAPEFILES = [
+    os.path.join(
+        RAW_DATA_DIR, "shapefiles", f"tl_2024_{a}_areawater",
+        f"tl_2024_{a}_areawater.shp"
+    )
+    for a in FIPS_DICT.keys()
+]
+
+# Tables
+P5_TABLE = os.path.join(RAW_DATA_DIR, "P5_race-hispanic", "DECENNIALDHC2020.P5-Data.csv")
