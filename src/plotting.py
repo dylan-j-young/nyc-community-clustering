933ef5f26f30053e9edfd7512dff697f2633e301
Dylan Young <dylan.young@colorado.edu>
2025-09-12 11:47:35 -0500

    Fixed aesthetics on plot_local_subregion()

--- src/plotting.py
+++ src/plotting.py
@@ -5,7 +5,68 @@ import contextily as ctx
 
 from src import config
 
-def plot_tracts(tracts, zoom_out="auto", zoom_adjust=0):
+def _plot_and_annotate(ax, tracts,
+                       linewidth: float = 1,
+                       fontsize: int = 6):
+    """
+    Basic wrapper for census tract plotting. Plot the polygons and annotate them.
+    """
+    # Add tracts to figure
+    tracts.plot(ax=ax,
+                facecolor="none",
+                edgecolor="black",
+                linewidth=linewidth
+               )
+
+    # Annotate with census tract numbers
+    for idx, row in tracts.iterrows():
+        # Label is Census Tract number
+        label = row["TRACT"]
+
+        # Label location is a representative point within the tract
+        point = row.geometry.representative_point()
+
+        ax.annotate(text=label, xy=(point.x, point.y),
+                    horizontalalignment="center",
+                    verticalalignment="center",
+                    fontsize=fontsize)
+    
+    return( ax )
+
+def _zoom(ax, zoom_out):
+    """
+    Basic wrapper for zooming out (or in) on a matplotlib axis.
+    
+    Parameters
+    ----------
+    ax : matplotlib.axes._axes.Axes
+        The matplotlib Axes object.
+
+    zoom_out : float
+        Sets a scale factor for zooming out the map from the census tract (higher = zoomed farther out). Useful if you want to see the broader region of a tract or small collection of tracts.
+
+    Returns
+    -------
+    ax : matplotlib.axes._axes.Axes
+        The matplotlib Axes object.
+    """    
+    # Zoom out: get current bounds and center
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+    x0, y0 = (xmin+xmax)/2, (ymin+ymax)/2
+    dx, dy = (xmax-xmin)/2, (ymax-ymin)/2
+
+    # Zoom out: set new bounds
+    xmin_new, xmax_new = x0 - dx*zoom_out, x0 + dx*zoom_out
+    ymin_new, ymax_new = y0 - dy*zoom_out, y0 + dy*zoom_out
+    ax.set_xlim(xmin_new, xmax_new)
+    ax.set_ylim(ymin_new, ymax_new)
+    
+    return( ax )
+
+def plot_tracts(tracts, 
+                zoom_out: float | str = "auto",
+                zoom_adjust: int = 0):
     """
     Quickly visualizes a subset of census tracts (or a single tract). For plotting, GeoDataFrames are locally converted to Web Mercator and then plotted on top of a contextily basemap.
 
@@ -40,42 +101,15 @@ def plot_tracts(tracts, zoom_out="auto", zoom_adjust=0):
     # Create fig, ax
     fig, ax = plt.subplots(figsize=(6, 4.5))
 
-    # Add tracts to figure
-    tracts.plot(ax=ax,
-                facecolor="none",
-                edgecolor="black",
-                linewidth=1
-               )
+    # Add tracts and census labels to figure
+    ax = _plot_and_annotate(ax, tracts)
     
-    # Zoom out: set defaults
+    # Zoom out: set defaults, or zoom out 
     if zoom_out == "auto":
         zoom_out = 1 if (len(tracts) > 1) else 5
-    assert not isinstance(zoom_out, str)
-    
-    # Zoom out: get current bounds and center
-    xmin, xmax = ax.get_xlim()
-    ymin, ymax = ax.get_ylim()
-    x0, y0 = (xmin+xmax)/2, (ymin+ymax)/2
-    dx, dy = (xmax-xmin)/2, (ymax-ymin)/2
-
-    # Zoom out: set new bounds
-    xmin_new, xmax_new = x0 - dx*zoom_out, x0 + dx*zoom_out
-    ymin_new, ymax_new = y0 - dy*zoom_out, y0 + dy*zoom_out
-    ax.set_xlim(xmin_new, xmax_new)
-    ax.set_ylim(ymin_new, ymax_new)
-
-    # Annotate with census tract numbers
-    for idx, row in tracts.iterrows():
-        # Label is Census Tract number
-        label = row["TRACT"]
-
-        # Label location is a representative point within the tract
-        point = row.geometry.representative_point()
-
-        ax.annotate(text=label, xy=(point.x, point.y),
-                    horizontalalignment="center",
-                    verticalalignment="center",
-                    fontsize=5)
+    else:
+        assert not isinstance(zoom_out, str)
+        ax = _zoom(ax, zoom_out)
     
     # Improve visualization
     ax.set_title("Census tracts", fontsize=12)
@@ -117,8 +151,8 @@ def plot_local_subregion(local_tracts, borough_tracts):
     local_tracts = local_tracts.to_crs(config.WEB_MERCATOR_EPSG)
     borough_tracts = borough_tracts.to_crs(config.WEB_MERCATOR_EPSG)
 
-    # Plot borough tracts
-    fig, ax = plot_tracts(borough_tracts, zoom_adjust=0)
+    # Create fig, ax
+    fig, ax = plt.subplots(figsize=(6, 4.5))
 
     # Shade in local tracts
     local_tracts.plot(ax=ax,
@@ -127,8 +161,27 @@ def plot_local_subregion(local_tracts, borough_tracts):
                 alpha=0.25
                 )
     
-    # Change title
-    ax.set_title("Local region tracts", fontsize=12)
+    # Zoom out to set xlim, ylim based off local_tracts
+    ax = _zoom(ax, 1.25)
+    
+    # Add borough tracts and census labels to figure
+    ax = _plot_and_annotate(ax, borough_tracts,
+                            linewidth=0.5,
+                            fontsize=4)
+    
+    # Improve visualization
+    ax.set_title("Local census tracts", fontsize=12)
+    ax.axis("off")  # Remove axis labels for a clean map
+    ax.set_aspect("equal")
+    fig.tight_layout()
+
+    # Add basemap
+    ctx.add_basemap(ax, 
+                    crs=config.WEB_MERCATOR_EPSG,
+                    source=ctx.providers.CartoDB.Positron,
+                    reset_extent=True,
+                    zoom_adjust=1
+                   )
     
     return( fig, ax )
 
