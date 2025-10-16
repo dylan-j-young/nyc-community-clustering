975d6c4b04c69c61efdcd6640f6de79ff4ef792e
Dylan Young <dylan.young@colorado.edu>
2025-10-15 23:35:24 -0500

    Added REDCAP and SKATER algorithms

--- src/algorithms.py
+++ src/algorithms.py
@@ -6,6 +6,9 @@ import geopandas as gpd
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from libpysal.weights import Rook
 from spopt.region import RegionKMeansHeuristic
+import pygeoda
+
+import warnings
 
 def kmeans(gdf, attrs, n, seed=None):
     """ 
@@ -142,4 +145,177 @@ def sc_agg(gdf, attrs, n, seed=None):
     model.fit(X)
 
     labels = pd.Series(model.labels_, index=gdf.index, name="labels")
+    return( labels )
+
+def redcap_full_ward(gdf, attrs, n, seed=0):
+    """ 
+    Wrapper for pygeoda's redcap algorithm, using full-order connectivity and Ward linkages for constructing the initial spanning tree. Given a GeoDataFrame of fully connected tracts, perform REDCAP and pull out the clustering labels.
+
+    Parameters
+    ----------
+    gdf : gpd.GeoDataFrame
+        Dataset to cluster. Must contain a valid geometry column in order to calculate contiguity.
+
+    attrs : list of str
+        List of the column names of gdf to be used in clustering.
+
+    n : int
+        Number of clusters to form. Must be at least 1.
+
+    seed : int, optional
+        Random seed fed into algorithm. Default is 0.
+
+    Returns
+    -------
+    labels : pd.Series
+        Series of the resulting cluster labels, indexed by the index in gdf. Name of the series is "labels".
+    """
+    # Data and contiguity matrix
+    data = gdf[attrs]
+    with warnings.catch_warnings():
+        # Warnings from pygeoda methods related to deprecated pandas stuff
+        warnings.filterwarnings("ignore")
+        geoda_obj = pygeoda.open(gdf)
+        rook_weights = pygeoda.rook_weights(geoda_obj)
+
+    ## -- Set up REDCAP options --
+    # Spanning tree generation options: 'firstorder-singlelinkage' (SKATER), 'fullorder-singlelinkage', 'fullorder-averagelinkage', 'fullorder-completelinkage', 'fullorder-wardlinkage'
+    method = "fullorder-wardlinkage"
+    distance_method = "euclidean" # Keep as Euclidean if using Ward
+    
+    # Setting a minimum cluster size = 5
+    bound_variable = np.ones(len(data)) # Each observation is assigned "1"
+    min_bound = 5 # min_cluster_size observations enforced
+
+    # Misc
+    random_seed = seed
+
+    # Perform clustering routine
+    redcap_dict = pygeoda.redcap(n, rook_weights, data,
+        method=method, distance_method=distance_method,
+        bound_variable=bound_variable, min_bound=min_bound,
+        random_seed=random_seed
+    )
+    
+    # Extract cluster labels from output dictionary
+    labels = pd.Series(
+        list(redcap_dict["Clusters"]),
+        index=gdf.index, name="labels"
+    )
+    labels -= 1 # Zero-index
+
+    return( labels )
+
+def redcap_full_average(gdf, attrs, n, seed=0):
+    """ 
+    Wrapper for pygeoda's redcap algorithm, using full-order connectivity and average linkages for constructing the initial spanning tree. Given a GeoDataFrame of fully connected tracts, perform REDCAP and pull out the clustering labels.
+
+    Parameters
+    ----------
+    gdf : gpd.GeoDataFrame
+        Dataset to cluster. Must contain a valid geometry column in order to calculate contiguity.
+
+    attrs : list of str
+        List of the column names of gdf to be used in clustering.
+
+    n : int
+        Number of clusters to form. Must be at least 1.
+
+    seed : int, optional
+        Random seed fed into algorithm. Default is 0.
+
+    Returns
+    -------
+    labels : pd.Series
+        Series of the resulting cluster labels, indexed by the index in gdf. Name of the series is "labels".
+    """
+    # Data and contiguity matrix
+    data = gdf[attrs]
+    with warnings.catch_warnings():
+        # Warnings from pygeoda methods related to deprecated pandas stuff
+        warnings.filterwarnings("ignore")
+        geoda_obj = pygeoda.open(gdf)
+        rook_weights = pygeoda.rook_weights(geoda_obj)
+
+    ## -- Set up REDCAP options --
+    # Spanning tree generation options: 'firstorder-singlelinkage' (SKATER), 'fullorder-singlelinkage', 'fullorder-averagelinkage', 'fullorder-completelinkage', 'fullorder-wardlinkage'
+    method = "fullorder-averagelinkage"
+    distance_method = "euclidean" # Keep as Euclidean if using Ward
+    
+    # Setting a minimum cluster size = 5
+    bound_variable = np.ones(len(data)) # Each observation is assigned "1"
+    min_bound = 5 # min_cluster_size observations enforced
+
+    # Misc
+    random_seed = seed
+
+    # Perform clustering routine
+    redcap_dict = pygeoda.redcap(n, rook_weights, data,
+        method=method, distance_method=distance_method,
+        bound_variable=bound_variable, min_bound=min_bound,
+        random_seed=random_seed
+    )
+    
+    # Extract cluster labels from output dictionary
+    labels = pd.Series(
+        list(redcap_dict["Clusters"]),
+        index=gdf.index, name="labels"
+    )
+    labels -= 1 # Zero-index
+
+    return( labels )
+
+def skater(gdf, attrs, n, seed=0):
+    """ 
+    Wrapper for pygeoda's skater algorithm. Given a GeoDataFrame of fully connected tracts, perform SKATER and pull out the clustering labels.
+
+    Parameters
+    ----------
+    gdf : gpd.GeoDataFrame
+        Dataset to cluster. Must contain a valid geometry column in order to calculate contiguity.
+
+    attrs : list of str
+        List of the column names of gdf to be used in clustering.
+
+    n : int
+        Number of clusters to form. Must be at least 1.
+
+    seed : int, optional
+        Random seed fed into algorithm. Default is 0.
+
+    Returns
+    -------
+    labels : pd.Series
+        Series of the resulting cluster labels, indexed by the index in gdf. Name of the series is "labels".
+    """
+    # Data and contiguity matrix
+    data = gdf[attrs]
+    with warnings.catch_warnings():
+        # Warnings from pygeoda methods related to deprecated pandas stuff
+        warnings.filterwarnings("ignore")
+        geoda_obj = pygeoda.open(gdf)
+        rook_weights = pygeoda.rook_weights(geoda_obj)
+
+    ## -- Set up REDCAP options --
+    distance_method = "euclidean"
+    random_seed = seed
+    
+    # Setting a minimum cluster size = 5
+    bound_variable = np.ones(len(data)) # Each observation is assigned "1"
+    min_bound = 5 # min_cluster_size observations enforced
+   
+    # Perform clustering routine
+    redcap_dict = pygeoda.skater(n, rook_weights, data,
+        distance_method=distance_method,
+        bound_variable=bound_variable, min_bound=min_bound,
+        random_seed=random_seed
+    )
+    
+    # Extract cluster labels from output dictionary
+    labels = pd.Series(
+        list(redcap_dict["Clusters"]),
+        index=gdf.index, name="labels"
+    )
+    labels -= 1 # Zero-index
+
     return( labels )
